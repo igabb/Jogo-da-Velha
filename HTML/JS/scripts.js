b56bcd9d5345e1ac7b6c8599d592c8958e8da7f8
iGabb <gb0104@hotmail.com>
2022-04-01 23:08:02 -0300

    Update scripts.js

---
+++ HTML/JS/scripts.js
@@ -0,0 +1,7 @@
+const celulas = document.querySelectorAll(".celula");
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
+
+document.addEventListener("click", (event) => {
+    console.log(event.target.id);
+})
